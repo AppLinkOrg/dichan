54382e0bc03152573913139cff4ff5cc7d27e2da
tao-zi-zi <844166094@qq.com>
2020-02-24 20:30:26 +0800

    11

---
+++ source/apis/city.api.js
@@ -0,0 +1,45 @@
+
+/****使用方法，下面两句复制到page的js文件的头部
+	
+import { ApiConfig } from '../../apis/apiconfig';
+import { InstApi } from '../../apis/inst.api';
+
+var instApi=new InstApi();
+*******/
+import { ApiConfig } from 'apiconfig';
+export class CityApi {
+  //获取首页的顶部横幅广告
+  citylist(json, callback, showLoading = true) {
+
+    if (showLoading)
+      ApiConfig.ShowLoading();
+
+    var header = ApiConfig.GetHeader();
+    console.log(header);
+    wx.request({
+      url: ApiConfig.GetApiUrl() + 'city/citylist',
+      data: json,
+      method: 'POST',
+      dataType: 'json',
+      header: header,
+      success: function (res) {
+        if (callback != null) {
+          callback(res.data);
+        }
+      },
+      fail: function (res) {
+        console.log(res);
+        callback(false);
+      },
+      complete: function (res) {
+        console.log(res);
+
+        if (showLoading)
+          ApiConfig.CloseLoading();
+      }
+    })
+  }
+
+
+}
+
--- source/pages/home/home.js
+++ source/pages/home/home.js
@@ -2,6 +2,7 @@
 import { AppBase } from "../../appbase";
 import { ApiConfig } from "../../apis/apiconfig";
 import { InstApi } from "../../apis/inst.api.js";
+import { CityApi } from "../../apis/city.api.js";
 
 class Content extends AppBase {
   constructor() {
@@ -11,13 +12,53 @@ class Content extends AppBase {
     this.Base.Page = this;
     //options.id=5;
     super.onLoad(options);
+    this.Base.setMyData({
+      StatusBar: getApp().globalData.StatusBar,
+      CustomBar: getApp().globalData.CustomBar,
+      Custom: getApp().globalData.Custom,
+      seq: 2,
+      showcity:false,
+      currentcity: {
+        id: 1,
+        name: "深圳"
+      },
+    })
   }
   onMyShow() {
     var that = this;
+    this.getcity();
+  }
+  getcity(){
+    var cityapi = new CityApi;
+    cityapi.citylist({ }, (citylist)=>{
+      console.log(citylist);
+      this.Base.setMyData({ citylist})
+    })
+  }
+  qiehuan(){
+    var showcity = this.Base.getMyData().showcity;
+    showcity = !showcity;
+    this.Base.setMyData({
+      showcity
+    })
+  }
+  switchcity(e){
+    console.log(e);
+    var cur = e.currentTarget.dataset.current;
+    var idx = e.currentTarget.id;
+    var seq = this.Base.getMyData().seq;
+    this.Base.setMyData({
+      currentcity:cur,
+      showcity:false,
+      seq: idx
+    })
   }
 }
 var content = new Content();
 var body = content.generateBodyJson();
 body.onLoad = content.onLoad;
 body.onMyShow = content.onMyShow;
+body.getcity = content.getcity;
+body.qiehuan = content.qiehuan;
+body.switchcity = content.switchcity;
 Page(body)
